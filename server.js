a3bb9957914509092eb2e4e348af5aad869b3f71
iYkjFQ5w9PcB4UpDRLeMrZ2zyWGv0O3T <77343162+iYkjFQ5w9PcB4UpDRLeMrZ2zyWGv0O3T@users.noreply.github.com>
2023-01-30 20:45:15 +0000

    ver2

--- server.js
+++ server.js
@@ -42,14 +42,14 @@ app.get("/listen", (req, res) => {
   });
 
 
-//获取节点数据
+//获取数据
 app.get("/list", (req, res) => {
     let cmdStr = "cat list";
     exec(cmdStr, function (err, stdout, stderr) {
       if (err) {
         res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
       } else {
-        res.type("html").send("<pre>节点数据：\n\n" + stdout + "</pre>");
+        res.type("html").send("<pre>数据：\n\n" + stdout + "</pre>");
       }
     });
   });
